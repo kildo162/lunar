2f7bf8662ac5748702fdafca3a68f6da6a32f600
KhanhND <khanhnd162@gmail.com>
2024-12-27 14:18:37 +0700

    minor update

---
+++ core/error.go
@@ -0,0 +1,55 @@
+package lunar
+
+import "fmt"
+
+type Error interface {
+	Code() string
+	Message() string
+	IsValid() bool
+}
+
+type errorData struct {
+	code    string
+	message string
+}
+
+func (e *errorData) IsValid() bool {
+	return e != nil && e.code == OK.Code()
+}
+
+func (e *errorData) Code() string {
+	return e.code
+}
+
+func (e *errorData) Error() string {
+	return fmt.Sprintf("Code:%s, Message: %s", e.code, e.message)
+}
+
+func (e *errorData) Message() string {
+	return e.message
+}
+
+func NewError(code string, message string) Error {
+	return &errorData{
+		code:    code,
+		message: message,
+	}
+}
+
+func IsError(err Error) bool {
+	return err != nil && !err.IsValid()
+}
+
+var (
+	OK                    = NewError("OK", "Success")
+	SERVER_ERROR          = NewError("ServerError", "Server Error")
+	BAD_REQUEST           = NewError("BadRequest", "Bad Request")
+	PERMISSION_ERROR      = NewError("PermissionError", "Permission Error")
+	REQUEST_INVALID       = NewError("RequestInvalid", "Request Invalid")
+	MODULE_NOT_EXISTS     = NewError("ModuleNotExists", "Module Not Exists")
+	BAD_DATA              = NewError("BadData", "Bad Data")
+	STREAM_ERROR          = NewError("StreamError", "Stream Error")
+	OBJECT_NOT_FOUND      = NewError("ObjectNotFound", "Object Not Found")
+	OBJECT_EXISTS         = NewError("ObjectExists", "Object Exists")
+	COMMAND_NOT_COMPLETED = NewError("CommandNotCompleted", "Command Not Completed")
+)
---
+++ core/go.mod
@@ -0,0 +1,3 @@
+module lunar/core
+
+go 1.23
